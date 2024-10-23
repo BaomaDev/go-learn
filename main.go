82ab851baa09c340ab83652b582cdfd0cbbdc5a0
lookingfrag <jessesimanjuntaks@gmail.com>
2024-10-24 06:16:19 +0700

    Data Types & Getting User Inpus

--- main.go
+++ main.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go Conference"
-	const confereceTickets = 50
-	var remainingTickets = 50
+	const conferenceTickets int = 50
+	var remainingTickets uint = 50
+	conferenceName := "Go Conference"
 
 	fmt.Println(" 	      _                           ")
 	fmt.Println("	     | |                          ")
@@ -16,8 +16,31 @@ func main() {
 	fmt.Println("                                          ")
 	fmt.Println("                                          ")
 	fmt.Println("=========================================")
-	fmt.Printf("Get your Ticket to Attend %v\n", conferenceName)
+	fmt.Printf("Welcome to %v booking application.\n", conferenceName)
 	fmt.Println("=========================================")
-	fmt.Printf("Total Tickets: %v Total Tickets Available: %v", confereceTickets, remainingTickets)
+	fmt.Printf("We have total of %v tickets and %v are still available.\nGet your tickets here to attend\n", conferenceTickets, remainingTickets)
 
+	//User Inputs
+	var firstName string
+	var lastName string
+	var email string
+	var userTickets uint
+
+	fmt.Println("Enter Your First Name: ")
+	fmt.Scanln(&firstName)
+
+	fmt.Println("Enter Your Last Name: ")
+	fmt.Scanln(&lastName)
+
+	fmt.Println("Enter Your Email: ")
+	fmt.Scanln(&email)
+
+	fmt.Println("Enter number of tickets: ")
+	fmt.Scanln(&userTickets)
+
+	// book ticket in system
+	remainingTickets = remainingTickets - userTickets
+
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will receive a confirmation email at %v\n", firstName, lastName, userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
